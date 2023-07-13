f6f52a7e5b37b1b26d9958fe90114db3f7c79c8b
jabad88 <abadiesjosh@gmail.com>
2023-07-12 17:51:35 -0700

    first commit

---
+++ stock_tracker.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import json
+import matplotlib.pyplot as plt
+import pendulum
+
+def main():
+    user_ticker = input("Please input stock Ticker: ").upper()
+    stock = yf.Ticker(user_ticker)
+
+    all_info = (stock.get_info())
+    current_market = all_info["regularMarketOpen"]
+    prev_close = all_info["regularMarketPreviousClose"] 
+
+    print (f"Market Price: {current_market}")
+    print(f"Previous Close Price: {prev_close}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
